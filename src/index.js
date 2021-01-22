f7158ebb2502624fb54dea75fd420921a9d060d8
hirschharald <hirschharald@gmx.net>
2021-01-22 09:52:47 +0100

    fall back to day before

--- src/index.js
+++ src/index.js
@@ -3,32 +3,49 @@ const _ = require("lodash");
 
 // get day and month for url
 var tag = new Date().getDate();
+var vortag = new Date().getDate() - 1;
 var monat = new Date().getMonth() + 1;
 if (tag < 10) tag = "0" + tag;
 if (monat < 10) monat = "0" + monat;
 
-const URL =
+const url =
   "https://www.mtk.org/statics/ds_doc/downloads/21-" +
   monat +
   tag +
   "_Coronazahlen.pdf";
 
-crawlpdf()
+const urlVorTag =
+  "https://www.mtk.org/statics/ds_doc/downloads/21-" +
+  monat +
+  vortag +
+  "_Coronazahlen.pdf";
+
+crawlpdf(url)
   .then((response) => {
     const newarr = response.toString().split(" ");
     console.log(newarr);
   })
-  .catch((err) => console.log(err));
+  .catch((err) =>
+    crawlpdf(urlVorTag)
+      .then((response) => {
+        const newarr = response.toString().split(" ");
+        console.log(newarr);
+      })
+      .catch(err => console.log(err))
+  );
 
-function crawlpdf() {
+function crawlpdf(url) {
   return new Promise(async function (resolve, reject) {
-    crawler(URL).then((response) => {
-      const arr = response.text.split("\n");
-
-      const kelkarr = arr.filter((itm) => {
-        return _.startsWith(itm, "Kelkheim");
-      });
-      resolve(kelkarr);
-    });
-  }).catch((err) => console.log(err));
+    crawler(url)
+      .then((response) => {
+        //console.log("xxxxx", response);
+        if (response.status == 404) reject(new Error(404));
+        const arr = response.text.split("\n");
+        const kelkarr = arr.filter((itm) => {
+          return _.startsWith(itm, "Kelkheim");
+        });
+        resolve(kelkarr);
+      })
+      .catch((err) => reject(err));
+  });
 }
